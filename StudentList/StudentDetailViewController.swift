27b9d6518adb4776f0a75df93ea7d506e04bca85
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-16 22:31:13 -0800

    finished student list app

---
+++ StudentList/StudentDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  StudentDetailViewController.swift
+//  StudentList
+//
+//  Created by Mehdi MMS on 16/02/2022.
+//
+
+import UIKit
+
+class StudentDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateViews()
+    }
+    
+    private func updateViews() {
+        guard let student = student, isViewLoaded else { return }
+        
+        firstNameLabel.text = student.firstName
+        lastNameLabel.text = student.lastName
+        ageLabel.text = "\(student.age)"
+
+    }
+    
+    var student: Student? {
+        didSet {
+            updateViews()
+        }
+    }
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+}
--- StudentList/StudentTableViewController.swift
+++ StudentList/StudentTableViewController.swift
@@ -16,6 +16,7 @@ class StudentTableViewController: UITableViewController {
     }
 
     override func tableView( _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell", for: indexPath)
         
         let student = StudentController.students[indexPath.row]
@@ -25,14 +26,17 @@ class StudentTableViewController: UITableViewController {
     }
    
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let tappedIndexPath = tableView.indexPathForSelectedRow else { return }
+        
+        let student = StudentController.students[tappedIndexPath.row]
+        if let detailVC = segue.destination as? StudentDetailViewController {
+            detailVC.student = student
+        }
     }
-    */
+    
 
 }
